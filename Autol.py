63acf035eb946d38ec59b32b7a2e63ec4883f697
User <solopenko2005@gmail.com>
2024-10-14 15:49:27 +0300

    Проба библиотеки numpy

--- Autol.py
+++ Autol.py
@@ -1,3 +1,7 @@
-print ("Вика")
-print("Степа")
-print("Вова")
+import openpyxl
+import pandas as pd
+ws = pd.read_excel('C:/Users/User/Desktop/МДТ/1.xlsx')
+ws_np = np.array(ws)
+
+# Сохранить файл
+wb.save("sample.xlsx")
